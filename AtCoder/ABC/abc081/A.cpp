8912bcdf38a6d36565361f550eace6de2280cb12
kamono-mafuyu <kamono-mafuyu@dmm.com>
2019-08-22 12:56:42 +0900

    Added abc081 A answer

--- AtCoder/ABC/abc081/A.cpp
+++ AtCoder/ABC/abc081/A.cpp
@@ -2,5 +2,14 @@
 using namespace std;
 
 int main() {
+    string s;
+    cin >> s;
 
+    int i = 0;
+    for (int j = 0; j < s.length(); ++j) {
+        if (s[j] == '1') {
+            i++;
+        }
+    }
+    cout << i;
 }
