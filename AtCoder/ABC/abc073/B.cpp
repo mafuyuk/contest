1eae7cb6ddaf0b95143b9f705f1ea12ed2eae7a4
kamono-mafuyu <kamono-mafuyu@dmm.com>
2019-08-22 19:10:25 +0900

    Added abc73 B case answer

---
+++ AtCoder/ABC/abc073/B.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+
+int main() {
+    int N;
+    cin >> N;
+
+    int sum;
+
+    rep(i, N) {
+        int l, r;
+        cin >> l >> r;
+
+        sum += r - l + 1;
+
+    }
+
+    cout << sum << endl;
+}
