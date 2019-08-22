a1dd54634a4b712ca32bb877d4097a05b7e640c6
kamono-mafuyu <kamono-mafuyu@dmm.com>
2019-08-22 19:03:27 +0900

    Added abc89 B case answer

--- AtCoder/ABC/abc089/B.cpp
+++ AtCoder/ABC/abc089/B.cpp
@@ -6,32 +6,20 @@ int main() {
     int N;
     cin >> N;
 
-    bool P = false;
-    bool W = false;
-    bool G = false;
     bool Y = false;
 
     rep(i, N) {
         string S;
         cin >> S;
 
-        if (S == "P") {
-            P = true;
-        } else if (S == "W") {
-            W = true;
-        } else if (S == "G") {
-            G = true;
-        } else if (S == "Y") {
+        if (S == "Y") {
             Y = true;
         }
     }
 
-    if (P && W && G) {
-        if (Y) {
-            cout << "Four" << endl;
-        } else {
-            cout << "Three" << endl;
-        }
-
+    if (Y) {
+        cout << "Four" << endl;
+    } else {
+        cout << "Three" << endl;
     }
 }
