3efcfb5da51e6f5f36afa2fd4f6a5cf3d06dda2a
kamono-mafuyu <kamono-mafuyu@dmm.com>
2019-08-22 14:37:14 +0900

    Added ex8 answer

---
+++ AtCoder/APG4b/ex8/Main.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int p;
+    cin >> p;
+
+    int price;
+    int N;
+
+    // パターン1
+    if (p == 1) {
+        cin >> price >> N;
+    }
+
+    // パターン2
+    if (p == 2) {
+        string text;
+
+        cin >> text >> price >> N;
+        cout << text << "!" << endl;
+    }
+
+    cout << price * N << endl;
+}
