f447f3d9f69311d2355cb69c1a7ef708c341aa67
kamono-mafuyu <kamono-mafuyu@dmm.com>
2019-09-12 21:09:53 +0900

    Added ex23

--- AtCoder/APG4b/ex23/Main.cpp
+++ AtCoder/APG4b/ex23/Main.cpp
@@ -1,7 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// TODO:
 int main() {
     int N;
     cin >> N;
@@ -11,10 +10,18 @@ int main() {
     }
 
     map<int, int> score;
-    for (int i; i < N; i++) {
-        score[A.at(i)] += 1;
+    for (int x : A) {
+        score[x] += 1;
     }
 
-    //sort(score.rbegin(), score.rend());
-    cout << score.at(0) << endl;
+    int max_cnt = 0;
+    int ans = -1;
+    for (int x : A) {
+        if (max_cnt < score.at(x)) {
+            max_cnt = score.at(x);
+            ans = x;
+        }
+    }
+
+    cout << ans << " " << max_cnt << endl;
 }
